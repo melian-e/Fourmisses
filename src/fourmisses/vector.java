dc0a539cfbae851ff3c7f66f80ea8818ec9656ef
Clément <clement.gorse@student.junia.com>
2022-11-02 10:54:45 +0100

    oui

---
+++ src/fourmisses/vector.java
@@ -0,0 +1,22 @@
+package fourmisses;
+
+abstract public class vector extends pos{
+
+	int speedX;
+	int speedY;
+	
+	public int getSpeedX() {
+		return speedX;
+	}
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+	public int getSpeedY() {
+		return speedY;
+	}
+	public void setSpeedY(int speedY) {
+		this.speedY = speedY;
+	}
+	
+	
+}
