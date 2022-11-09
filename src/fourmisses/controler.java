15357ba1763a9a7d8be8446091b7e3e0bb4f1f1c
Clément <clement.gorse@student.junia.com>
2022-11-09 17:27:10 +0100

    test

--- src/fourmisses/controler.java
+++ src/fourmisses/controler.java
@@ -1,9 +1,22 @@
 package fourmisses;
 
 public class Controler {
-	//initialisation  obstacles, nourriture, class player
+	
+	/*initialisation  obstacles, nourriture, class player*/
 
 	Player player1;
 	Player player2;
+	Obstacle obstacle;
 	
+	
+
+	public Controler() {
+		// les variables eront calculées par des méthodes auparavant (symétrie notamment)
+		int coordColony1[] = {40,40};
+		//création obstacle et bouffe
+		
+		//symétrie
+		this.player1 = new Player( 0, 3, 0, 5, 1, coordColony1[0], coordColony1[1]);
+		this.player2 = new Player(0, 3, 0, 5, 1, 5, 5);
+	}
 }
