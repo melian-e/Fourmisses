cfc3897b2b01ab09d4ab7f9573ce15c497c12c57
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2022-11-16 19:57:18 +0100

    Maj initialisation du model

--- src/fourmisses/Model.java
+++ src/fourmisses/Model.java
@@ -10,26 +10,39 @@ public class Model {
 
 	Player player1;
 	Player player2;
+	int[] gameHeightWidth = {1900, 1000};
 	List<Obstacle> tabObstacle;
 	
 	
 	
 
 	public Model() {
-		// les variables eront calculées par des méthodes auparavant (symétrie notamment)
-		int coordColony1[] = {40,40};
-		//création obstacle et bouffe
-		List<List<Integer>> tabCoords = new ArrayList<List<Integer>>();
-		for(int i = 0; i<100; i++) {
-			int randomx = (int)Math.random();
-			int randomy = (int)Math.random();
-			List<Integer> tmp = new ArrayList<Integer>();
-			tmp.add(randomx);
-			tmp.add(randomy);
-			tabCoords.add(tmp);
+		// les variables seront calculées par des méthodes auparavant (symétrie notamment)
+		int totalObstacle = 30;
+		
+		//création coordonnées colony, obstacle et bouffe
+		double coordColony1[] = {40,40};
+		List<double[]> tabCoordsObstacle = new ArrayList<double[]>();
+		for(int i = 0; i<totalObstacle/2; i++) {
+			double randomx = Math.random()*gameHeightWidth[0];
+			double randomy = Math.random()*gameHeightWidth[1];
+			double tmp[] = {randomx,randomy};
+			tabCoordsObstacle.add(tmp);
 		}
+		
 		//symétrie
+		double coordColony2[] = {gameHeightWidth[0] - coordColony1[0], gameHeightWidth[1] - coordColony1[1]};
+		for(int i = 0; i<totalObstacle/2; i++) {
+			double[] tmp = tabCoordsObstacle.get(i);
+			tmp[0] = gameHeightWidth[0] - tmp[0];
+			tmp[1] = gameHeightWidth[1] - tmp[1];
+			tabCoordsObstacle.add(tmp);
+		}
+		
+		
+		
+		//création des instances
 		this.player1 = new Player( 0, 3, 0, 5, 1, coordColony1[0], coordColony1[1]);
-		this.player2 = new Player(0, 3, 0, 5, 1, 5, 5);
+		this.player2 = new Player(0, 3, 0, 5, 1, coordColony2[0], coordColony2[1]);
 	}
 }
